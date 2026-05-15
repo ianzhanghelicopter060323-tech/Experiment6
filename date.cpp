35b52e9954712df1df447bbde5e02407724b173d
Ian Zhang <ianzhanghelicopter060323@gmail.com>
2026-05-15 21:05:05 +0800

    add Date.cpp

---
+++ date.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Date
+{
+    private:
+        int year;  // BC: negative; AD: positive
+        int month; // can be 1~12
+        int day;   // can be 1~28, 1~29, 1~30, 1~31
+    public:
+
+};
